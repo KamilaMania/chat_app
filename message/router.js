2d68ed250b892be3f17c9a52c4afe9ad1ad83b73
KamilaMania <mania.kamika@icloud.com>
2019-12-13 11:22:45 +0100

    add catch, async

--- message/router.js
+++ message/router.js
@@ -6,13 +6,21 @@ const { Router } = express;
 const router = new Router();
 
 router.get("/message", async (request, response, next) => {
-  const messages = await Message.findAll();
-  response.send(messages);
+  try {
+    const messages = await Message.findAll();
+    response.send(messages);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/message", async (request, response, next) => {
-  const message = await Message.create(request.body);
-  response.send(message);
+  try {
+    const message = await Message.create(request.body);
+    response.send(message);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
